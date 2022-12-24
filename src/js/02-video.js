501d7a1f9306e98dbe4ee3fe1b3d6821895fc096
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2022-12-25 01:44:05 +0200

    add 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,21 @@
 import Player from '@vimeo/player';
-//console.log(Player);
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+console.log(player);
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(e.seconds));
+  }, 2500)
+);
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0)
+  .catch(function (error) {
+    console.error('error');
+  });
